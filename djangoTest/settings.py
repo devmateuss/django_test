f22a83fa9d4fa89d87074a03b8264ff6bf8f6439
Mateus Sousa <mateus.sousa@deway.com.br>
2022-02-22 07:49:54 -0300

    add alow host

--- djangoTest/settings.py
+++ djangoTest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xatvb8_%1c6paw*71ncm52&tr75nmni6_2ng1)652wc+$%7$!7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
